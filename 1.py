8e1ec5afa9d9d23c7c4348251cbc59bf88b2c75b
Aditya <aditya.aparadh.0@gmail.com>
2021-09-04 12:46:11 +0530

    This is how do we do Grids in Tkinter

--- 1.py
+++ 1.py
@@ -3,6 +3,9 @@ from tkinter import *
 root = Tk()
 
 #This creates a label widget
-theLabel = Label(root, text="Hello world")
-theLabel.pack()
+theLabel1 = Label(root, text="Hello world")
+theLabel2 = Label(root, text="My name is Aditya. Im doing Python")
+
+theLabel1.grid(row=0, column=0)
+theLabel2.grid(row=1, column=1)
 root.mainloop()
