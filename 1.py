df51c65f98e59088b54c6ba67ca149a1385ed6e6
Aditya <aditya.aparadh.0@gmail.com>
2021-09-04 12:33:16 +0530

    Initial commit

---
+++ 1.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+
+#This creates a label widget
+theLabel = Label(root, text="Hello world")
+theLabel.pack()
+root.mainloop()
---
+++ pyton executer.bat
@@ -0,0 +1,4 @@
+@echo off
+1.py
+pause 
+exit
