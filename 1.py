126074e6bcef7952e4be4a23e214e4878e7c639a
Aditya <aditya.aparadh.0@gmail.com>
2021-09-04 14:24:45 +0530

    minor grammar issue

--- 1.py
+++ 1.py
@@ -32,7 +32,7 @@ myButton = Button(root, text="Button", padx=200, pady= 200, command = buttonClic
 myButton.grid(row=0, column=1)
 
 #Here padx and pady are used to define the size of buttons in pixels
-#Here command is used to define the command to execute in case the button is clicked
+#Here "command" is used to define the command to execute in case the button is clicked
 #bg and fg are used to define background and foreground color respectively for the button
 
 
